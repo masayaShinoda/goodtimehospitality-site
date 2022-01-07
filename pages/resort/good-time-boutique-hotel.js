7ae8ff511d5f0271dbcb2b66cdd794a9a53c5afe
Masaya Shida <masayashida@gmail.com>
2022-01-07 22:34:12 +0700

    JSX fix

--- pages/resort/good-time-boutique-hotel.js
+++ pages/resort/good-time-boutique-hotel.js
@@ -17,7 +17,7 @@ export default function GoodTimeBoutiqueHotel() {
                         src="/images/goodtimeboutiquehotel-logo_result.webp" 
                         alt="Good Time Relax Resort logo" 
                     />
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FGOODTIMEBOUTIQUEHOTEL&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="500" height="500" style={{border: `none`, overflow: `hidden`}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FGOODTIMEBOUTIQUEHOTEL&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="500" height="500" style={{border: `none`, overflow: `hidden`}} scrolling="no" frameBorder="0" allowFullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                 </div>
                 {
                     typeof window !== 'undefiend' && (
