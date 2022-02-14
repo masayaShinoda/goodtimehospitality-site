8a6f7d6fcdfbeef26e7bd15f9cae0e29cc410ae1
Masaya Shida <masayashida@gmail.com>
2022-02-14 13:19:01 +0700

    update about page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,71 @@
 import Layout from '../components/layout'
+import Link from 'next/link'
+import styles from '../styles/about.module.scss'
 
 export default function About() {
+    const gtgh_logo = "images/logo-goodtimegrouphospitality.png"
+    const fbIcon = '/icons/fb-icon-margins.png'
+    const igIcon = '/icons/ig-icon.png'
 
     return (
-        <Layout pageName="About">
-            <span style={{padding: `50px 0`, display: `flex`, flexDirection: `column`, alignItems: `center`}}>
-                <h1 style={{marginBottom: `1.4rem`}}>About</h1>
-                <p>Page is under construction. Stay tuned!</p>
+    <Layout pageName="About">
+    <div className={styles.aboutPageContainer}>
+        <div className={styles.aboutContent}>
+            {/* <h1>Good Time Hospitality Group</h1> */}
+            <div className={styles.div_1}>
+                <img src={gtgh_logo} alt="Good Time Hospitality Group logo" />      
+            </div>
+            <div className={styles.div_2}>
+                <blockquote className={styles.blockquote}>
+                <p>
+                Our properties have distinctive look, making use of the untouched land in Cambodia. We aim to make our guests remember every second of their stay.                    
+                </p>
+                </blockquote>
+                <span>
+                    <Link href="https://www.facebook.com/goodtimerelaxresort/">
+                        <a 
+                        id="fb_link"
+                        title="Like us on Facebook"
+                        className={styles.social_link}
+                        style={{
+                            display: `inline-flex`, flexDirection: `row`, alignItems: `center`,
+                            color: `#fff`, borderRadius: `100%`,
+                            margin: `0 .5rem`, transition: `all 150ms ease-in`,
+                        }}>
+                            <img 
+                            src={fbIcon} 
+                            alt="facebook icon" 
+                            width="40"
+                            height="40"
+                            />
 
-            </span>
+                        </a>
+                    </Link>
+                    <Link href="https://www.instagram.com/goodtimehospitalitygroup/">
+                        <a 
+                        id="fb_link"
+                        title="Like us on Facebook"
+                        className={styles.social_link}
+                        style={{
+                            display: `inline-flex`, flexDirection: `row`, alignItems: `center`,
+                            color: `#fff`, borderRadius: `100%`,
+                            margin: `0 .5rem`, transition: `all 150ms ease-in`,
+                        }}>
+                            <img 
+                            src={igIcon} 
+                            alt="facebook icon" 
+                            width="40"
+                            height="40"
+                            />
 
-        </Layout>
+                        </a>
+                    </Link>
+                </span>
+            </div>
+
+        </div>
+
+    </div>
+    </Layout>
     )
 }
--- pages/good-time-boutique-hotel.js
+++ pages/good-time-boutique-hotel.js
@@ -3,41 +3,46 @@ import Layout from '../components/layout'
 import styles from '../styles/subpage.module.scss';
 
 export default function GoodTimeBoutiqueHotel() {
+    const fb_iframe_width = "500"
+    const fb_iframe_height = "500"
 
     return (
-        <>
-            <Layout>
-                <div className={styles.background}>
-                    <img src="images/gt-br-choice_result.webp" alt="background" />
-                </div>
-                <div className={styles.subpageContainer}>
-                    <img 
-                        width="280"
-                        height="280"
-                        src="images/goodtimeboutiquehotel-logo_result.webp" 
-                        style={{filter: `grayscale(100%) brightness(10)`}}
-                        alt="Good Time Relax Resort logo" 
-                    />
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FGOODTIMEBOUTIQUEHOTEL&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" 
-                    width="500" 
-                    height="500" 
-                    style={{border: `none`, overflow: `hidden`}} 
-                    scrolling="no" 
-                    frameBorder="0" 
-                    allowFullScreen="true" 
-                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-                </div>
-                {
-                    typeof window !== 'undefiend' && (
-                        <MessengerComponent
-                        pageId="111918520338184"
-                        // appId="<APP_ID>"
-                        // htmlRef="<REF_STRING>"
-                        />
-                    )
-                }
+    <>
+        <Layout>
+            <div className={styles.gt_bh_background}>
+                <img src="images/gt-br-choice_result.webp" alt="background" />
+            </div>
+            <div className={styles.subpageContainer}>
+                <div className={styles.gt_bh}>
 
-            </Layout>
-        </>
+                <img 
+                    width="280"
+                    height="280"
+                    src="images/goodtimeboutiquehotel-logo_result.webp" 
+                    style={{filter: `grayscale(100%) brightness(10)`}}
+                    alt="Good Time Relax Resort logo" 
+                />
+                <iframe src={`https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FGOODTIMEBOUTIQUEHOTEL&tabs=timeline&width=${fb_iframe_width}&height=${fb_iframe_height}&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId`}
+                width={fb_iframe_width}
+                height={fb_iframe_height}
+                style={{border: `none`, overflow: `hidden`}} 
+                scrolling="no" 
+                frameBorder="0" 
+                allowFullScreen={true}
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                </div>
+            </div>
+            {/* {
+                typeof window !== 'undefiend' && (
+                    <MessengerComponent
+                    pageId="111918520338184"
+                    // appId="<APP_ID>"
+                    // htmlRef="<REF_STRING>"
+                    />
+                )
+            } */}
+
+        </Layout>
+    </>
     )
 }
